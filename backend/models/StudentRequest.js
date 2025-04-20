5d137a2a5fde929b9f6774729eb34c370455dfec
Bar Alon <baral4@ac.sce.ac.il>
2025-04-20 18:42:02 +0300

    BSPM25T16-48 - staff request view

---
+++ backend/models/StudentRequest.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const studentRequestSchema = new mongoose.Schema({
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  staff: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  requestType: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'RequestType', 
+    required: true
+  },
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true
+  },
+  
+
+  description: String,
+  documents: [{
+    documentName: String,
+    documentURL: String
+  }],
+  department: String,
+  status: {
+    type: String,
+    enum: ["ממתין", "בטיפול", "אושר", "נדחה"],
+    default: "ממתין"
+  },
+  submissionDate: {
+    type: Date,
+    default: Date.now
+  },
+  staffComments: [{
+    staff: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    comment: String,
+    date: { type: Date, default: Date.now }
+  }]
+});
+
+module.exports = mongoose.model('StudentRequest', studentRequestSchema);
