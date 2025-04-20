5f456de8705d972cef65a91ead8125c57afc4846
Bar Alon <baral4@ac.sce.ac.il>
2025-04-20 18:42:50 +0300

    BSPM25T16-2 + BSPM25T16-12 + BSPM25T16-13 + BSPM25T16-48 – login and staff request view

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: false ,
+  
+  }
+
+});
+module.exports = mongoose.model("User", userSchema);
