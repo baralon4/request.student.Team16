470d00aca8d1605fd0e339a93326c3eb0831e15b
Bar <baral4@ac.sce.ac.il>
2025-04-25 13:30:05 +0300

    BSPM25T16-166,BSPM25T16-169,BSPM25T16-172,BSPM25T16-184

--- backend/tests/back.test.js
+++ backend/tests/back.test.js
@@ -1,31 +1,83 @@
 const request = require("supertest");
-const { app, INIT, server } = require("../index");
+const { app, startServer } = require("../index");
 const mongoose = require("mongoose");
+const User = require("../models/User");
 
-describe("api", () => {
-  beforeAll(async () => {
-    await INIT.didInit;
+let server;
+
+beforeAll(async () => {
+  server = await startServer();
+
+  await User.create({
+    id: "123123123",
+    firstname: "Test",
+    lastname: "User",
+    username: "testuser",
+    password: "testpass",
+    role: "Admin",
+    department: "Testdepart",
   });
+});
 
-  afterAll(async () => {
-    await mongoose.connection.close();
+afterEach(async () => {
+  await User.deleteMany({ username: { $in: ["testuser"] } });
+});
 
-    if (server) {
-      await new Promise((resolve) => {
-        server.close(() => {
-          console.log("Server closed");
-          resolve();
-        });
+afterAll(async () => {
+  await mongoose.connection.close();
+
+  if (server) {
+    await new Promise((resolve) => {
+      server.close(() => {
+        console.log("Server closed");
+        resolve();
       });
-    }
+    });
+  }
+});
+
+describe("integration Test", () => {
+  test("Successful login with correct credentials", async () => {
+    const res = await request(app)
+      .post("/api/login")
+      .send({ username: "testuser", password: "testpass" })
+      .expect(200);
+
+    expect(res.body.success).toBe(true);
+    expect(res.body.user).toMatchObject({
+      firstname: "Test",
+      lastname: "User",
+      username: "testuser",
+      role: "Admin",
+      department: "Testdepart",
+    });
   });
 
-  test("test jenkins2", async () => {
+  test("Failed login with incorrect credentials", async () => {
+    const res = await request(app)
+      .post("/api/login")
+      .send({ username: "testuser", password: "worngpass" })
+      .expect(401);
+
+    expect(res.body.success).toBe(false);
+    expect(res.body.message).toBe("Invalid credentials");
+  });
+
+  test("returns list of student requests for user 'staff1'", async () => {
     const response = await request(app)
       .get("/api/staff/requests?staffUsername=staff1")
       .set("user-role", "Staff")
       .expect("Content-Type", /json/);
 
-    console.log(response.body);
+    console.log("Requests for staff1:", response.body);
+  });
+
+  test("returns list of student requests for user 'staff2'", async () => {
+    const response = await request(app)
+      .get("/api/staff/requests?staffUsername=staff2")
+      .set("user-role", "Staff")
+      .expect("Content-Type", /json/);
+
+    console.log("Requests for staff2:", response.body);
   });
 });
