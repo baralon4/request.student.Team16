1495856110589fda20fa7eea3a81e175a4f12ad9
baralon4 <161120348+baralon4@users.noreply.github.com>
2025-04-25 20:00:30 +0300

    test log out BSPM25T16-178 BSPM25T16-178 BSPM25T16-181

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,61 +1,59 @@
+import * as services from "./utils/services";
 
-import * as services from './utils/services'
-
-describe('Services', () => {
-
-
-  describe('saveToLocalStorage', () => {
-    test('save string value to local storage', () => {
-      const key = 'example'
-      services.saveToLocalStorage(key, '1')
-      expect(localStorage.getItem(key)).toBe('1');
+describe("Services", () => {
+  describe("saveToLocalStorage", () => {
+    test("save string value to local storage", () => {
+      const key = "test1";
+      services.saveToLocalStorage(key, "1");
+      expect(localStorage.getItem(key)).toBe("1");
     });
 
-    test('stringify object and save local storage', () => {
-      const key = 'example2'
-      const exampleObject = { key: 'value' }
-      services.saveToLocalStorage(key, exampleObject)
+    test("stringify object and save local storage", () => {
+      const key = "test2";
+      const exampleObject = { key: "value" };
+      services.saveToLocalStorage(key, exampleObject);
       expect(localStorage.getItem(key)).toBe(JSON.stringify(exampleObject));
     });
-  })
+  });
 
-  describe('getFromLocalStorage', () => {
-
-    test('return null if nothing is in the local storage', () => {
-      expect(services.getFromLocalStorage('some-key')).toBe(null);
+  describe("getFromLocalStorage", () => {
+    test("return null if nothing is in the local storage", () => {
+      expect(services.getFromLocalStorage("some-key")).toBe(null);
     });
 
-    test('return parsed local storage value', () => {
-      const key = 'some-key'
-      localStorage.setItem(key, JSON.stringify({ example: 1 }))
+    test("return parsed local storage value", () => {
+      const key = "some-key";
+      localStorage.setItem(key, JSON.stringify({ example: 1 }));
 
       const result = services.getFromLocalStorage(key);
-      expect(result).toMatchObject({ example: 1 })
+      expect(result).toMatchObject({ example: 1 });
     });
-  })
+  });
 
-  describe('logout', () => {
+  describe("logout", () => {
+    test("removes user data from localStorage", () => {
+      localStorage.setItem(
+        services.USER_DATA_LOCAL_STORAGE_KEY,
+        JSON.stringify({ key: "test1" })
+      );
+      expect(
+        services.getFromLocalStorage(services.USER_DATA_LOCAL_STORAGE_KEY)
+      ).toMatchObject({ key: "test1" });
 
-    test('removes user data from localStorage', () => {
-
-      localStorage.setItem(services.USER_DATA_LOCAL_STORAGE_KEY, JSON.stringify({ key: 'example' }))
-      expect(services.getFromLocalStorage(services.USER_DATA_LOCAL_STORAGE_KEY)).toMatchObject({ key: 'example' });
-
-      services.logout()
-      expect(services.getFromLocalStorage(services.USER_DATA_LOCAL_STORAGE_KEY)).toBe(null);
-
+      services.logout();
+      expect(
+        services.getFromLocalStorage(services.USER_DATA_LOCAL_STORAGE_KEY)
+      ).toBe(null);
     });
-  })
+  });
 
-  describe('Validators', () => {
-
-    test('exists', () => {
-
+  describe("Validators", () => {
+    test("exists", () => {
       expect(services.Validators.exists()).toBe(false);
       expect(services.Validators.exists(undefined)).toBe(false);
       expect(services.Validators.exists(null)).toBe(false);
-      expect(services.Validators.exists('')).toBe(false);
-      expect(services.Validators.exists('example')).toBe(true);
+      expect(services.Validators.exists("")).toBe(false);
+      expect(services.Validators.exists("test1")).toBe(true);
     });
-  })
-})
+  });
+});
